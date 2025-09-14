24c2b0f52893e25ac797171d7bb7458c1ce79eb1
NormalFall <93881091+NormalFall@users.noreply.github.com>
2025-09-13 21:33:20 -0400

    Add nix files

---
+++ default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "jellyfin-rpc";
+  version = "1.3.3";
+
+  src = fetchFromGitHub {
+    owner = "Radiicall";
+    repo = "jellyfin-rpc";
+    rev = version;
+    hash = "sha256-zKqP6Wt38ckqCPDS1oncmx92lZJm2oeb3bfpwVc6fUc=";
+  };
+
+  cargoHash = "sha256-k9dGz+1HGcQoDMyqmJ1hEYklfYHibo1PI5jHEe0mr+w=";
+
+  meta = {
+    description = "Displays the content you're currently watching on Discord";
+    homepage = "https://github.com/Radiicall/jellyfin-rpc";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "jellyfin-rpc";
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Flake for jellyfin-rpc";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+flake-utils.lib.eachDefaultSystem (system:
+  let
+    overlays = [ (import rust-overlay) ];
+    pkgs = import nixpkgs {
+      inherit system overlays;
+    };
+  in {
+    packages.x86_64-linux.default = pkgs.callPackage ./default.nix {};
+
+    devShells.default = pkgs.mkShell {
+      buildInputs = [ (pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" ];
+      }) ];
+    };
+  });
+}
